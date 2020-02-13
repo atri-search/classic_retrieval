e45c03d81e5df9e4d0a7332e3075c1c502cd2dc9
marcosfpr <mfprezende@gmail.com>
2020-02-12 22:06:21 -0300

    Travis CI test

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/marcosfpr/match_up_lib",
-    packages=setuptools.find_packages(exclude=['tests']),
+    packages=setuptools.find_packages(exclude=['tests', 'docs']),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
